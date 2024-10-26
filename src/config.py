242643b948fb2fce5dbf7242c3c79cfea8a3a5a3
Miriam Del Rayo Aguayo López <miriamaglop20@gmail.com>
2024-10-26 10:32:09 -0600

    API - Flask

---
+++ src/config.py
@@ -0,0 +1,10 @@
+class DevelopmentConfig():
+    DEBUG = True
+    MySQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'api_utl'
+
+config={
+    'development' : DevelopmentConfig
+}
