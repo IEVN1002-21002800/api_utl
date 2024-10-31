d2065e8c1fbc4121a7036d9722e75b2cd49cd939
Miriam Del Rayo Aguayo López <miriamaglop20@gmail.com>
2024-10-30 20:51:07 -0600

    Segundo Commit - INSERT

--- src/app.py
+++ src/app.py
@@ -26,8 +26,51 @@ def lista_alumnos():
     except Exception as ex:
         return jsonify({"message":"Error al conectar con base de datos {}".format(ex), 'exito':False})
     
+def leer_alumno_bd(matricula):
+    try:
+        cursor=con.connection.cursor()
+        sql='select * from alumnos where matricula={0}'.format(matricula)
+        cursor.execute(sql)
+        datos=cursor.fetchone()
+        if datos!=None:
+            alumno={'matricula':datos[0], 'nombre':datos[1], 'apaterno':datos[2], 'amaterno':datos[3], 'correo':datos[4]}
+            return alumno
+        else:
+            return None
+    except Exception as ex:
+        return jsonify({"message":"Error al conectar con base de datos {}"})
+
+@app.route("/alumnos/<mat>", methods = ['GET'])
+def leer_alumno(mat):
+    try:
+        alumno=leer_alumno_bd(mat)
+        if alumno!=None:
+            return jsonify({'alumnos':alumno, 'mensaje':'Lista Alumnos', 'exito':True})
+        else:
+            return jsonify({'mensaje':'Alumno no encontrado', 'exito': False})
+        
+    except Exception as ex:
+        return jsonify({"message":"Error al conectar con base de datos {}".format(ex), 'exito':False})
+    
 def pagina_no_encontrada(error):
     return "<h1> La pagina que estas buscando no existe </h1>",400
+
+@app.route("/alumnos", methods = ['POST'])
+def registrar_alumno():
+    try:
+            alumno = leer_alumno_bd(request.json['matricula'])
+            if alumno!= None:
+                return jsonify({'mensaje':'Alumnos ya existe', 'exito': False})
+            else:
+                cursor = con.connection.cursor()
+                sql = '''INSERT INTO alumnos (matricula,nombre,apaterno,amaterno,correo) 
+                values('{0}','{1}','{2}','{3}','{4}')'''.format(request.json['matricula'],request.json['nombre'],request.json['apaterno'],request.json['amaterno'],request.json['correo'])
+                cursor.execute(sql)
+                con.connection.commit()
+                return jsonify({'mensaje':'Alumno Agregado', 'exito':True})
+    except Exception as ex:
+            return jsonify({"message":"Error al conectar con base de datos {}".format(ex), 'exito':False})
+    
 
 if __name__=="__main__":
     app.config.from_object(config['development'])
